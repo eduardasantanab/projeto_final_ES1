5517280c29eac482c65412cb2fd4e4ccc874f503
Eduarda Barros <eduarda.santanasb@gmail.com>
2024-06-04 17:14:25 -0300

    adiciona teste automatizado para primeiro caso de uso

--- cypress/e2e/testes-es1.cy.js
+++ cypress/e2e/testes-es1.cy.js
@@ -1,5 +1,15 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('como professor, eu gostaria de poder visualizar meus dados pessoais no meu perfil', () => {
+  it('o usuário deve conter um cadastro de professor com suas informações de login.', () => {
     cy.visit('https://projeto-final-es-1.vercel.app/')
-  })
+
+    cy.contains('Login Professor')
+
+    cy.get('input[type="email"]').type('professora@gmail')
+    cy.get('input[type="password"]').type('0000')
+
+    cy.get('#professor_login_button').click()
+
+    //cy.get('#login').click()
+    })
 })
+
