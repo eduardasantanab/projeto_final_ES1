11b5779180a7f90432859831b1e4765db8a9c661
isadoravrx <isavitoriaxavier@gmail.com>
2024-06-06 11:52:12 -0300

    adicionando foto de perfil no register e em informacoes pessoais

--- common_dados_login_perfil.js
+++ common_dados_login_perfil.js
@@ -1,4 +1,4 @@
-async function showDados(nome,sobrenome,cpf,cep,email) {
+async function showDados(nome,sobrenome,cpf,cep,email,foto_perfil_url) {
     const s = nome + " " + sobrenome;
     document.getElementById("nome_sobrenome").innerHTML = s
     document.getElementById("nome").innerHTML = nome
@@ -6,6 +6,5 @@ async function showDados(nome,sobrenome,cpf,cep,email) {
     document.getElementById("email").innerHTML = email
     document.getElementById("cep").innerHTML = cep
     document.getElementById("cpf").innerHTML = cpf
-    //
-
+    document.getElementById("foto_perfil").src = foto_perfil_url;
 }
